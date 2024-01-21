568f22dfef75a455788dedebf151cefa1c08e653
yusei0211 <saito31126@icloud.com>
2024-01-21 21:04:56 +0900

    ログイン機能実装

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+class Authenticate extends Middleware
+{
+    protected $user_route = 'user.login';
+    protected $owner_route = 'owner.login';
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     */
+    protected function redirectTo(Request $request): ?string
+    {
+        if (! $request->expectsJson()){
+            if(Route::is('owner.*')){
+                return route($this->owner_route);
+            }else{
+                return route($this->user_route);
+            }
+        }
+    }
+}
